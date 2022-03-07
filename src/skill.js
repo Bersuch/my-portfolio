448ef0559d5e80625b3cf27027f3a1b31be7e441
Daniel <bersuchdaniel@yahoo.de>
2022-03-07 13:03:41 +0100

    Added skills list

--- src/skill.js
+++ src/skill.js
@@ -1,14 +1,18 @@
 export const skills = [
     {
+        id: 1,
         skill: "Java",
     },
     {
+        id: 2,
         skill: "C",
     },
     {
+        id: 3,
         skill: "Javascript",
     },
     {
+        id: 4,
         skill: "SQL"
     }
 ]
